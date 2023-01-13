2df0b44f97f2be3d5b25a8772e3f70e53e2bfb85
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-13 04:55:23 -0800

    Fix bug Iconbackgroumd

--- src/Components/IconBackground.jsx
+++ src/Components/IconBackground.jsx
@@ -1,65 +1,40 @@
 import React from 'react'
 
-const IconBackgroundSvg = (Icon) => {
-    
-    return (
-        <>
-            <div className='flex bg-gray-100 text-gray-600 border border-gray-200 p-1.5 rounded-md cursor-pointer hover:text-gray-500 duration-300 z-50' onClick={Icon.click}>
-                {Icon.children}
-            </div>
-
-        </>
-    )
-}
-
 export default function Iconheader(Data) {
-
-    const removeItem = () => {
-        const allIcon = document.querySelectorAll('.IconHeaderItem');
-
-        allIcon.forEach((item) => {
-
-            if (!item.classList.contains('hidden')) {
-                item.classList.add('hidden')
-            }
-        })
-    }
 
     const changeClassIcon = (e) => {
 
-        if (e.target.classList.contains('flex')) return;
+        // Receive parent elements
+        const element = e.target.parentElement.parentElement.childNodes;
 
-        const element = e.target.parentElement.parentElement.parentElement.childNodes;
+        // Checking the point that the user clicked
+        if (element[2]) {
+            if (element[2].classList.contains('IconHead')) return
+        }
+        else return
 
+        // Remove "Hidden" class for all Icons
+        const allIcon = document.querySelectorAll('.IconHeaderItem');
+        allIcon.forEach((item) => (!item.classList.contains('hidden')) ? item.classList.add('hidden') : null)
+
+        // show & hide items
         if (element[2].classList.contains('hidden')) {
-
-            removeItem()
-
             element[2].classList.remove('hidden')
-
         } else {
-
-            removeItem()
-
             element[2].classList.add('hidden')
         }
     }
+
     return (
         <>
-            <div id='IconHead' className="relative">
+            <div id='IconHead' className="IconHead relative" >
+
+                <div className='IconBackgroundSvg flex bg-gray-100 text-gray-600 border border-gray-200 p-1.5 rounded-md cursor-pointer hover:text-gray-500 duration-300 z-50' onClick={(Data.clickable) ? changeClassIcon : null}>
+                    {Array.isArray(Data.children) ? Data.children[0] : Data.children}
+                </div>
+
                 {
-                    Data.clickable
-                        ? <IconBackgroundSvg click={changeClassIcon}>
-                            {Array.isArray(Data.children) ? Data.children[0] : Data.children}
-                        </IconBackgroundSvg>
-
-                        : <IconBackgroundSvg>
-                            {Array.isArray(Data.children) ? Data.children[0] : Data.children}
-                        </IconBackgroundSvg>
-                }
-
-
-                {Data.counter &&
+                    Data.counter &&
                     <span className={`absolute ${Data.bgColor} text-white rounded-full -top-1 -right-1 p-1 px-1 p1-1.5 text-[0.5rem] leading-none`}>
                         {Data.counter}
                     </span>
