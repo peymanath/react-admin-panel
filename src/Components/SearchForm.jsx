06bb7c295a72d2486d17e8bfa4f43e53183730ac
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-13 05:16:04 -0800

    change icon search form

--- src/Components/SearchForm.jsx
+++ src/Components/SearchForm.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Search from '@mui/icons-material/Search';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 export default function SearchForm() {
+
+    const goToSearch = () => {
+
+    }
+
     return (
         <>
-            <form action='/search' className="hidden md:flex relative bg-[#F2F4F6] py-1 rounded-md border border-input text-[#616262]">
+            <form onSubmit={goToSearch} action='/search' className="hidden md:flex relative bg-[#F2F4F6] py-1 rounded-md border border-input text-[#616262]">
 
                 <input type="text" name="search" placeholder="جستجو" className="bg-transparent w-full h-full ml-7 pr-4" />
 
                 <button className="absolute top-1/2 -translate-y-1/2 left-2">
-                    <Search></Search>
+                    <MagnifyingGlassIcon className='w-5 h-5' />
                 </button>
 
             </form>
