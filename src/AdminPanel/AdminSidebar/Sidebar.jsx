c152a7b11d9859ba7e514d8424d28ac626eb0979
Peyman (ATH) Naderi <peymanath@gmail.com>
2023-01-13 02:29:58 -0800

    Change Color

--- src/AdminPanel/AdminSidebar/Sidebar.jsx
+++ src/AdminPanel/AdminSidebar/Sidebar.jsx
@@ -8,12 +8,12 @@ export default function Sidebar(Data) {
 
 
     return (
-        <aside className={`sidebar-menu overflow-y-auto hidden h-full md:flex md:flex-col bg-c-gray text-white ${Data.Active ? 'md:w-[70px]' : 'md:w-[200px]'}`}>
-            <header className="bg-primary">
+        <aside className={`sidebar-menu overflow-y-auto hidden h-full md:flex md:flex-col bg-gray-900 text-white ${Data.Active ? 'md:w-[70px]' : 'md:w-[200px]'}`}>
+            <header className=" bg-green-500">
 
                 <div className="flex py-3 px-4">
 
-                    <div className={`flex items-center ${Data.Active ? 'justify-center' : 'justify-between'} bg-c-gray text-white w-full rounded-md`}>
+                    <div className={`flex items-center ${Data.Active ? 'justify-center' : 'justify-between'} bg-gray-900 hover:bg-gray-800 duration-300 transition-colors text-white w-full rounded-md`}>
 
                         <div onClick={Data.click} className="btn-menu flex py-1.5 px-2 cursor-pointer">
 
@@ -21,11 +21,11 @@ export default function Sidebar(Data) {
 
                         </div>
 
-                        <Link key="Pabel" to="/" className={`${Data.Active ? 'hidden' : 'flex'} items-center justify-between gap-x-2 py-1.5 px-2 hover:bg-[#283543] duration-300 transition-colors text-sm w-3/4`}>
+                        <Link key="Pabel" to="/" className={`${Data.Active ? 'hidden' : 'flex'} items-center justify-between gap-x-2 py-1.5 px-2 text-sm w-3/4`}>
 
                             <span className="hidden sm:flex">پنل کاربری</span>
 
-                            <ProfileImage imgClassName="w-6 h-6 rounded-full" imgAlt="عکس پروفایل"/>
+                            <ProfileImage imgClassName="w-6 h-6rounded-full" imgAlt="عکس پروفایل"/>
 
                         </Link>
 
